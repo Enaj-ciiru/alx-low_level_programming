1265dc6954554ebeda989284f37c5888c32d68db
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-10 06:28:47 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,13 +8,8 @@ include<stdio.h>
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
+		puts("\"Programming is like building a multilingual puzzle")
 			return (0);
-
 }
