ee614cecd8c592223a45a3085b10d9c232399778
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-11 10:42:39 -0800

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -prints alphabet in lowercase
+ * new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
