9452b1806f6a3987c1f8eda415aa5e806a92820f
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-11 15:07:17 -0800

    all combos here

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,19 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 48; m <= 57; m++)
+		for (m = 49; m <= 57; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
+			if (m > n)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
