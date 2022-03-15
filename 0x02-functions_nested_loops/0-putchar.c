c40135790bd59d880aca314e4ce9ee68228052c0
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-15 02:57:39 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * new line
+ * Return: Always return 0
+ *
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
