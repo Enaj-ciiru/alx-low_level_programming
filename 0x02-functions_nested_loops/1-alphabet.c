7ac73f2c3ca9d8b832a2572fc2dee0d7aecfd161
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-15 04:19:54 -0700

    print increament lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
