18ad66dfc48d492a4842c6fcc522f77605afd4e2
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-16 18:25:53 -0700

    prints the first 98 Fibonacci numbers starting with 1 and 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the first 98 Fibonacci numbers
+ * starting with 1 and 2
+ * new line
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long int a = 1, b = 2, next = 0;
+
+	while (i < 98)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+		if (i < 97)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
