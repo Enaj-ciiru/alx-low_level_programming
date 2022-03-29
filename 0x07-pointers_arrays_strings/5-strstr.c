a5961dd2dfc7593da307e4e72b218be13b1ec4d6
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-03-28 18:05:26 -0700

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,12 +18,15 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-			if (haystack[i + j] != needle[j])
-				break;
-			if (!needle[j])
-				return (&haystack[i]);
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+				if (!needle[j])
+					return (&haystack[i]);
+			}
 		}
 	}
 	return (NULL);
